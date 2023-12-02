aacf3c1af9d468a52fb71ccbd31d2a5723a45b36
haseeb <pkhaseeb04@gmail.com>
2023-12-01 23:58:09 -0800

    while loop

---
+++ script.mjs
@@ -0,0 +1,90 @@
+// dishes =[]
+// for(let a=0;a<=5;a++)
+// {
+//     favdsh = prompt("tell me you favourite dish name or press q to quit")
+//     if(favdsh!='q'){
+//         dishes.push(favdsh)
+//     }
+//     else{
+//         break
+//     }
+//     dishes.push(favdsh)
+// }
+// console.log(dishes)
+
+// while lopp
+
+// counter = 0
+// while(counter <=10){
+//     square = counter **2
+//     if(square>100){
+//         break
+//     }
+//     else {
+//         console.log(counter**2)
+//         counter++
+//     }
+// }
+
+// dishes =[]
+// flag = true
+// while(flag){
+//     dishname = prompt("enter dishname or press q to quit")
+//     if (dishname !='q'){
+//     dishes.push(dishname)
+// }
+// else{
+//     flag = false
+//    // break
+// }
+// }
+// console.log(dishes)
+
+// var a= 10
+// var b = 20
+// while(b>a){
+//     console.log("bbbbb")
+//     a=a+10
+// }
+// flag = true
+// while (flag){
+//     console.log("bbbb")
+//     flag=false
+
+// }
+//output
+//>>>>>bbbb
+// while(true){
+//     console.log("BBBB")
+//     break
+// }
+//output
+//BBBBB
+//
+
+// Function
+// function addition(){
+// a=10
+// b=20
+// c=a+b
+// console.log(c)
+// }
+// addition()
+function addition(a,b){
+    c=a+b
+    return c
+    }
+ answer = addition(2,7)
+ console.log(answer)
+ 
+ answer = addition(3,7)
+  console.log(answer)
+
+ answer = addition(24,7)  
+ console.log(answer)
+ 
+
+  answer = addition(4,7)
+  console.log(answer)
+
+
